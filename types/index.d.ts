5af7a3b7b13b6d0ec97bc7c7ab971148fc3ba1dc
Maxime Kubik <mkubik@tftlabs.com>
2023-07-05 15:41:35 +0200

    feat: add basic types to init the project

---
+++ types/index.d.ts
@@ -0,0 +1,25 @@
+
+
+export type NavItem = {
+  title: string
+  href: string
+  disabled?: boolean
+}
+
+export type MainNavItem = NavItem
+
+
+export type SiteConfig = {
+  name: string
+  description: string
+  url: string
+  ogImage: string
+  links: {
+    twitter: string
+    github: string
+  }
+}
+
+export type LandingConfig = {
+  mainNav: MainNavItem[]
+}
