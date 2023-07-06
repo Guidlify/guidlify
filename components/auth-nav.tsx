09f394eac08109b5c95657664ac8fd3b6fd4983b
Imam Ali Mustofa <d.darkterminal@gmail.com>
2023-07-06 09:13:11 +0700

    feat: add AuthNav component

--- components/auth-nav.tsx
+++ components/auth-nav.tsx
@@ -1,31 +1,47 @@
 "use client"
 
+import React from "react"
 import Link from "next/link"
-import { usePathname, useRouter } from "next/navigation"
-import { NavItem } from "@/types"
+import { useRouter } from "next/navigation"
+import {
+  createClientComponentClient,
+  type Session,
+} from "@supabase/auth-helpers-nextjs"
 
 import { cn } from "@/lib/utils"
-import { buttonVariants } from "@/components/ui/button"
+import { Button, buttonVariants } from "@/components/ui/button"
 
-interface AuthNavProps {
-  items?: NavItem[]
-}
-
-export default function AuthNav({ items }: AuthNavProps) {
-  const pathname = usePathname()
+export default function AuthNav({ session }: { session: Session | null }) {
   const router = useRouter()
+  const supabase = createClientComponentClient()
+
+  const handleSignOut = async () => {
+    await supabase.auth.signOut()
+    router.refresh()
+  }
 
   return (
     <nav>
-      <Link
-        href="/login"
-        className={cn(
-          buttonVariants({ variant: "default", size: "sm" }),
-          "px-4"
-        )}
-      >
-        Login
-      </Link>
+      {session ? (
+        <>
+          <Button variant={"ghost"} size={"sm"}>
+            {session.user.email}
+          </Button>
+          <Button onClick={handleSignOut} variant={"secondary"} size={"sm"}>
+            Logout
+          </Button>
+        </>
+      ) : (
+        <Link
+          href={"/login"}
+          className={cn(
+            buttonVariants({ variant: "secondary", size: "sm" }),
+            "px-4"
+          )}
+        >
+          Login
+        </Link>
+      )}
     </nav>
   )
 }
