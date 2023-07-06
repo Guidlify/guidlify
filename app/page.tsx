54c21aa8138bc8fb5554b9b6ce9e64486af2b40c
Imam Ali Mustofa <d.darkterminal@gmail.com>
2023-07-06 09:14:04 +0700

    feat(page): add session handling and conditional rendering of login button

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,22 @@
+import { cookies } from "next/headers"
 import Link from "next/link"
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
 
-import { buttonVariants } from "@/components/ui/button"
+import { Database } from "@/types/supabase"
 import { siteConfig } from "@/config/site"
 import { cn } from "@/lib/utils"
+import { buttonVariants } from "@/components/ui/button"
 
-export default function IndexPage() {
+export default async function IndexPage() {
+  const supabase = createServerComponentClient<Database>({ cookies })
+  const {
+    data: { session },
+  } = await supabase.auth.getSession()
+
   return (
     <>
       <section className="grow space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-32">
-        <div className="container flex max-w-[64rem] flex-col justify-center items-center gap-4 text-center">
+        <div className="container flex max-w-[64rem] flex-col items-center justify-center gap-4 text-center">
           <Link
             href={siteConfig.links.twitter}
             className="rounded-2xl bg-muted px-4 py-1.5 text-sm font-medium"
@@ -20,12 +28,18 @@ export default function IndexPage() {
             WebXGuild 🎩
           </h1>
           <p className="max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
-            Where developers guild meet. Create your events, request sponsorship, and more.
+            Where developers guild meet. Create your events, request
+            sponsorship, and more.
           </p>
           <div className="space-x-4">
-            <Link href="/login" className={cn(buttonVariants({ size: "lg" }))}>
-              Login
-            </Link>
+            {!session ? (
+              <Link
+                href="/login"
+                className={cn(buttonVariants({ size: "lg" }))}
+              >
+                Login
+              </Link>
+            ) : null}
             <Link
               href={siteConfig.links.github}
               target="_blank"
@@ -37,6 +51,6 @@ export default function IndexPage() {
           </div>
         </div>
       </section>
-      </>
+    </>
   )
 }
