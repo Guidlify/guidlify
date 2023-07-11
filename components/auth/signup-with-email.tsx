70787f1f4d65064d643ec54e63b9d33726f2262f
Imam Ali Mustofa <d.darkterminal@gmail.com>
2023-07-11 22:07:48 +0700

    feat: create signup screen with email and password

---
+++ components/auth/signup-with-email.tsx
@@ -0,0 +1,69 @@
+"use client"
+
+import React, { FormEvent, useState } from "react"
+import { useRouter } from "next/navigation"
+
+import supabaseClient from "@/lib/supabase-browser"
+
+import { Button } from "../ui/button"
+import { Input } from "../ui/input"
+import { ToastAction } from "../ui/toast"
+import { useToast } from "../ui/use-toast"
+
+export default function SignUpWithEmail() {
+  const { toast } = useToast()
+  const router = useRouter()
+
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleSignUp = async (e: FormEvent) => {
+    e.preventDefault()
+    const signUp = await supabaseClient.auth.signUp({
+      email,
+      password,
+      options: {
+        emailRedirectTo: `${location.origin}/auth/callback`,
+      },
+    })
+    if (signUp.data.session === null) {
+      toast({
+        variant: "destructive",
+        title: "Whoops! You are already here.",
+        description: `Your email ${email} is already registered!`,
+        action: (
+          <ToastAction
+            altText="Try Login"
+            onClick={() => router.push("/login")}
+          >
+            Try Login
+          </ToastAction>
+        ),
+      })
+    }
+  }
+
+  return (
+    <form
+      className={`mx-auto mt-24 flex w-4/5 flex-col gap-3 transition-opacity duration-500`}
+      onSubmit={(e: FormEvent) => handleSignUp(e)}
+      autoComplete="off"
+    >
+      <h2 className="text-center text-lg font-bold">Sign Up to WebXGuild 🎩</h2>
+      <Input
+        name="email"
+        placeholder="Enter your email"
+        onChange={(e) => setEmail(e.target.value)}
+        value={email}
+      />
+      <Input
+        type="password"
+        name="password"
+        placeholder="Enter your password"
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}
+      />
+      <Button type="submit">Sign Up</Button>
+    </form>
+  )
+}
