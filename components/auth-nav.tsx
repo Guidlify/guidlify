6d2e141a78fef45668d471cbc0f4e01cf4781f2f
Imam Ali Mustofa <d.darkterminal@gmail.com>
2023-07-11 16:18:20 +0700

    refactor: Update import statements and variable names in components/auth-nav.tsx

--- components/auth-nav.tsx
+++ components/auth-nav.tsx
@@ -1,14 +1,12 @@
 "use client"
 
-import React, { useCallback, useEffect, useState } from "react"
+import { useCallback, useEffect, useState } from "react"
 import Link from "next/link"
 import { useRouter } from "next/navigation"
-import {
-  createClientComponentClient,
-  type Session,
-} from "@supabase/auth-helpers-nextjs"
+import { type Session } from "@supabase/auth-helpers-nextjs"
 
 import { ProfileState } from "@/types/users"
+import supabaseClient from "@/lib/supabase-browser"
 import { cn, getCountryFlag } from "@/lib/utils"
 import { Button, buttonVariants } from "@/components/ui/button"
 
@@ -16,21 +14,25 @@ import { Skeleton } from "./ui/skeleton"
 
 export default function AuthNav({ session }: { session: Session | null }) {
   const router = useRouter()
-  const supabase = createClientComponentClient()
   const [detail, setDetail] = useState<ProfileState>()
 
   const handleSignOut = useCallback(async () => {
-    await supabase.auth.signOut()
+    await supabaseClient.auth.signOut()
     setDetail(undefined)
     router.refresh()
-  }, [router, supabase])
+  }, [router])
 
   useEffect(() => {
     const loadProfile = async (id: string | undefined) => {
-      const userProfile = await fetch(`/api/user/profile?id=${id}`)
-      const { data: profile } = await userProfile.json()
-      setDetail(profile)
+      try {
+        const userProfile = await fetch(`/api/user/profile?id=${id}`)
+        const { data: profile } = await userProfile.json()
+        setDetail(profile)
+      } catch (error) {
+        console.error("Error loading profile:", error)
+      }
     }
+
     // eslint-disable-next-line react-hooks/exhaustive-deps
     if (detail === undefined && session !== null) {
       loadProfile(session?.user.id)
