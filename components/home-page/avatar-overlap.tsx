075e9ff1441f2ad60a79869916d6644a71a59e0a
Anmol-Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-10-08 19:01:20 +0530

    chore: add dynamic variation size prop

--- components/home-page/avatar-overlap.tsx
+++ components/home-page/avatar-overlap.tsx
@@ -1,33 +1,45 @@
 import Image from "next/legacy/image"
 
-const AvatarOverLap = () => {
+interface AvatarOverLapProps {
+  size?: number
+}
+
+const AvatarOverLap = ({ size = 36 }: AvatarOverLapProps) => {
+  const containerSize = size / 4 + 2
+
   return (
     <div className="mt-2 flex flex-col items-center justify-center">
       <div className="flex">
-        <div className="z-2 relative flex h-11 w-11 items-center justify-center rounded-full bg-violet-500">
+        <div
+          className={`z-2 h-${containerSize} w-${containerSize} relative flex items-center justify-center rounded-full bg-violet-500`}
+        >
           <Image
             src={"/avatars/photo-1.avif"}
             alt=""
-            width={36}
-            height={36}
+            width={size}
+            height={size}
             className="rounded-full"
           />
         </div>
-        <div className="z-3 relative ml-[-16px] flex h-11 w-11 items-center justify-center rounded-full bg-violet-500">
+        <div
+          className={`z-3 h-${containerSize} w-${containerSize} relative ml-[-10px] flex items-center justify-center rounded-full bg-violet-500`}
+        >
           <Image
             src={"/avatars/photo-3.avif"}
             alt=""
-            width={36}
-            height={36}
+            width={size}
+            height={size}
             className="rounded-full"
           />
         </div>
-        <div className="z-4 relative ml-[-16px] flex h-11 w-11 items-center justify-center rounded-full bg-violet-500">
+        <div
+          className={`z-4 h-${containerSize} w-${containerSize} relative ml-[-10px] flex items-center justify-center rounded-full bg-violet-500`}
+        >
           <Image
             src={"/avatars/photo-4.avif"}
             alt=""
-            width={36}
-            height={36}
+            width={size}
+            height={size}
             className="rounded-full"
           />
         </div>
