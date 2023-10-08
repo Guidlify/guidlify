1d9aa7e1342172f05f1cd00df7d3b245b418a3b4
Anmol-Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-10-08 23:34:43 +0530

    chore: add check tools section

---
+++ components/home-page/check-tools.tsx
@@ -0,0 +1,49 @@
+import Image from "next/legacy/image"
+import { ArrowRight, Check } from "lucide-react"
+
+const CheckTools = () => {
+  const features = [
+    "Launch a crowdfunding campaign to turn your event into reality",
+    "Collaborate with the community to make your event successful",
+    "Guidlify is dedicated to Open Source and the Community",
+    "Everything you need to kickstart your event",
+  ]
+
+  return (
+    <div className="flex flex-row px-8 py-2">
+      <div className="flex flex-col items-center lg:w-1/2">
+        <Image
+          alt="tutorial image preview"
+          src={"/video-preview.png"}
+          width={998 / 2.2}
+          height={634 / 2.2}
+          className="transition-all duration-500 hover:opacity-40"
+        />
+      </div>
+      <div className="flex items-center justify-center px-4 lg:w-1/2">
+        <div className="flex flex-col space-y-2">
+          <h1 className="mb-4 text-2xl font-medium">
+            An all-in-one Event Hub for event organizers, sponsors, and
+            attendees
+          </h1>
+          {features.map((feature, index) => (
+            <div className="flex flex-row" key={index}>
+              <span className="mr-3 text-sm">
+                <Check />
+              </span>
+              {feature}
+            </div>
+          ))}
+          <div className="hover-move-icon flex cursor-pointer flex-row pt-4 text-violet-500">
+            <span>Check the tools</span>
+            <span className="icon pl-2">
+              <ArrowRight />
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default CheckTools
