c7a169093c56841df1e2fe93cf95346e099b7a9f
Maxime Kubik <mkubik@tftlabs.com>
2023-10-11 16:43:53 +0200

    feat: update footer to match figma

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -1,48 +1,80 @@
 import * as React from "react"
 
-import { siteConfig } from "@/config/site"
-import { cn } from "@/lib/utils"
 import { Icons } from "@/components/icons"
 import { ModeToggle } from "@/components/mode-toggle"
+import { cn } from "@/lib/utils"
+import Image from "next/image"
 
 export function SiteFooter({ className }: React.HTMLAttributes<HTMLElement>) {
-  return (
-    <footer className={cn(className)}>
-      <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
-        <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
-          <Icons.radio />
-          <p className="text-center text-sm leading-loose md:text-left">
-            Built by{" "}
-            <a
-              href={siteConfig.links.twitter}
-              target="_blank"
-              rel="noreferrer"
-              className="font-medium underline underline-offset-4"
-            >
-              WebXGuild
-            </a>
-            . Hosted on{" "}
-            <a
-              href="https://vercel.com"
-              target="_blank"
-              rel="noreferrer"
-              className="font-medium underline underline-offset-4"
-            >
-              Vercel
-            </a>
-            . Wanna help us? Feel free to contribute on{" "}
-            <a
-              className="font-semibold underline"
-              href="https://github.com/WebXGuild/guidlify.com"
-            >
-              GitHub
-            </a>
-            .
-          </p>
-        </div>
+	const footerData = [
+		{
+			title: "About",
+			items: ["Company", "Store", "Open Source", "Research"],
+		},
+		{
+			title: "Resources",
+			items: ["Tools", "Blog", "Tutorials", "FAQs"],
+		},
+		{
+			title: "Support",
+			items: ["Contact Us", "Developers", "Documentation", "Integrations"],
+		},
+		{
+			title: "Legal",
+			items: ["Policy", "Terms", "Privacy", "Cookies"],
+		},
+	]
+	return (
+		<footer className={cn(className)}>
+			<div className="container flex flex-col items-center justify-between gap-4 md:flex-row md:py-0">
 
-        <ModeToggle />
-      </div>
-    </footer>
-  )
+
+				<div className="flex flex-col items-center lg:items-start gap-4 md:flex-row md:gap-2 md:px-0 flex-1">
+
+					<div className="xs:col-span-5 xs:pb-8 lg:col-span-1 lg:pb-0">
+						<Image
+							src={"/guildify-brand-logo.svg"}
+							alt=""
+							width={120}
+							height={50}
+						/>
+					</div>
+
+					<div className="w-full mx-auto flex flex-wrap gap-4 justify-between items-center px-4 pb-4">
+						{footerData.map((column, index) => (
+							<div
+								key={index}
+								className="flex flex-col xs:w-full sm:w-auto sm:flex-shrink-0 xs:col-span-5 xs:text-center sm:col-span-2 sm:pb-0 sm:text-left lg:col-span-1 gap-2"
+							>
+								<p className="font-semibold">{column.title}</p>
+								{column.items.map((item, itemIndex) => (
+									<p
+										key={itemIndex}
+										className="text-sm md:text-md cursor-pointer transition duration-200 hover:underline"
+									>
+										{item}
+									</p>
+								))}
+							</div>
+						))}
+					</div>
+				</div>
+				<ModeToggle />
+			</div>
+
+			<div className="container flex flex-col items-center justify-between gap-4 md:flex-row  md:border-t md:border-muted-foreground py-4">
+				<h1 className="flex px-6">
+					<div className="mr-2">
+						<Icons.Discord className="h-6 w-6 text-[#5865F2]" />
+					</div>
+					<div className="mr-2">
+						<Icons.X className="h-5 w-5" />
+					</div>
+				</h1>
+				<h1 className="xs:text-md px-6 sm:text-sm">
+					Copyright © 2023 Guidlify
+				</h1>
+			</div>
+		</footer>
+	)
 }
