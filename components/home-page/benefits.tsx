336c8308a4cac52e3212afbf095adc9296ceb422
Maxime Kubik <mkubik@tftlabs.com>
2023-10-11 16:44:18 +0200

    fix: removing padding to fit into main container

--- components/home-page/benefits.tsx
+++ components/home-page/benefits.tsx
@@ -1,68 +1,65 @@
 import Image from "next/legacy/image"
 
 const Benefits = () => {
-  const benefitData = [
-    {
-      imageSrc: "benefits-1.png",
-      title: "Event Management",
-      description:
-        "Platea arcu dapibus non magna cursus lectus id sollicitudin. Enim viverra parturient tristique nulla.",
-    },
-    {
-      imageSrc: "benefits-2.png",
-      title: "Funding Opportunity",
-      description:
-        "Congue mauris sem vel, urna viverra. Urna, nibh leo suscipit purus ut sed eros, consectetur viverra.",
-    },
-    {
-      imageSrc: "benefits-3.png",
-      title: "Community Engagement",
-      description:
-        "Posuere quis sed mauris non curabitur pretium elementum eget. Feugiat sed maecenas eu accumsan tristique.",
-    },
-    {
-      imageSrc: "benefits-4.png",
-      title: "Event Metrics",
-      description:
-        "Sit bibendum donec dolor fames neque vulputate non sit aliquam. Consequat turpis natoque leo, massa.",
-    },
-  ]
+	const benefitData = [
+		{
+			imageSrc: "benefits-1.png",
+			title: "Event Management",
+			description:
+				"Platea arcu dapibus non magna cursus lectus id sollicitudin. Enim viverra parturient tristique nulla.",
+		},
+		{
+			imageSrc: "benefits-2.png",
+			title: "Funding Opportunity",
+			description:
+				"Congue mauris sem vel, urna viverra. Urna, nibh leo suscipit purus ut sed eros, consectetur viverra.",
+		},
+		{
+			imageSrc: "benefits-3.png",
+			title: "Community Engagement",
+			description:
+				"Posuere quis sed mauris non curabitur pretium elementum eget. Feugiat sed maecenas eu accumsan tristique.",
+		},
+		{
+			imageSrc: "benefits-4.png",
+			title: "Event Metrics",
+			description:
+				"Sit bibendum donec dolor fames neque vulputate non sit aliquam. Consequat turpis natoque leo, massa.",
+		},
+	]
 
-  return (
-    <div className="m-12 p-12">
-      <div className="text-center xs:mb-16 xs:text-xs sm:mb-8 md:text-left">
-        <h1 className="mb-2 font-semibold xs:text-2xl md:text-3xl">
-          Enhance Benefits
-        </h1>
-        <h3 className="font-light md:text-lg">
-          Collaborate with the community to make your event successful
-        </h3>
-      </div>
-      <div className="mt-12 grid grid-cols-1 gap-10 sm:grid-cols-2 md:grid-cols-4">
-        {benefitData.map((benefit, idx) => (
-          <div key={idx} className="max-w-40 mx-auto sm:mx-0">
-            <div className="mb-4">
-              <div className="items-center justify-center xs:flex sm:justify-start">
-                <Image
-                  alt={`benefits icon for subcategory ${idx}`}
-                  src={`/home-page/benefits-section/${benefit.imageSrc}`}
-                  width={100}
-                  height={100}
-                  className=""
-                />
-              </div>
-              <div className="mt-4 items-center text-center sm:text-left">
-                <h1 className="xs:text-md mb-2 font-semibold tracking-wider md:text-lg">
-                  {benefit.title}
-                </h1>
-                <h3 className="xs:text-xs md:text-sm">{benefit.description}</h3>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  )
+	return (
+		<div className="mx-auto flex max-w-[64rem] flex-col">
+			<div className="text-center xs:mb-16 sm:mb-8 md:text-left">
+				<h2 className="font-heading text-lg tracking-wide sm:text-lg md:text-xl lg:text-3xl">
+					Enhance Benefits
+				</h2>
+				<h3 className="text-balance flex-wrap leading-normal text-muted-foreground sm:text-md sm:leading-8">
+					Collaborate with the community to make your event successful
+				</h3>
+			</div>
+
+			<div className="flex flex-col md:flex-row gap-4">
+				{benefitData.map((benefit, idx) => (
+					<div key={idx} className="flex-1 flex items-center sm:items-start justify-center xs:flex sm:justify-start flex-col">
+						<Image
+							alt={`benefits icon for subcategory ${idx}`}
+							src={`/home-page/benefits-section/${benefit.imageSrc}`}
+							width={75}
+							height={75}
+							className="w-full h-full object-contain"
+						/>
+						<div className="mt-4 items-center text-center sm:text-left">
+							<h1 className="xs:text-md mb-2 font-semibold tracking-wider md:text-md">
+								{benefit.title}
+							</h1>
+							<h3 className="flex-wrap leading-normal text-muted-foreground sm:leading-8">{benefit.description}</h3>
+						</div>
+					</div>
+				))}
+			</div>
+		</div>
+	)
 }
 
 export default Benefits
