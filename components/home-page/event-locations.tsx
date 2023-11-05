85db179a99c146dfa5f1e1bdd6b9d92dbf895cef
Imam Ali Mustofa <d.darkterminal@gmail.com>
2023-11-05 16:51:20 +0700

    refactor(event-locations): change component structures

--- components/home-page/event-locations.tsx
+++ components/home-page/event-locations.tsx
@@ -68,44 +68,49 @@ const EventLocations = () => {
   }, [])
 
   return (
-    <div className="event-locations-container">
+    <div id="infinite" className="slider">
       {Array.from({ length: numRows }).map((_, rowIndex) => (
-        <div
-          key={rowIndex}
-          className={`row row-${rowIndex} flex flex-row space-x-5 pb-10`}
-        >
-          {shuffledData[rowIndex]?.map((item, index) => (
-            <div
-              className="card relative shrink-0 transition-all duration-300 hover:scale-105 hover:cursor-pointer hover:shadow-slate-800"
-              key={index}
-            >
-              <Image
-                alt={item.alt}
-                src={`/home-page/event-locations/${item.src}`}
-                height={303}
-                width={463}
-                className="opacity-60"
-                priority={true}
-              />
-              <div className="card-content absolute inset-x-0 bottom-[-2px] bg-gradient-to-b from-transparent to-white px-6 pb-10 text-black dark:to-[#030711]">
-                {item.heading === undefined ? (
-                  <div className="text-md flex pb-2 font-semibold tracking-wide md:text-lg">
-                    <span className="pr-2">
-                      <ChevronsRight />
-                    </span>{" "}
-                    {item.subtext}
+        <div key={rowIndex} className={`row-${rowIndex} barrier`}>
+          <ul className="lane">
+            {shuffledData[rowIndex]?.map((item, index) => (
+              <>
+                <li className="event-location">
+                  <div
+                    className="card relative shrink-0 transition-all duration-300 hover:scale-105 hover:cursor-pointer hover:shadow-slate-800"
+                    key={index}
+                  >
+                    <Image
+                      alt={item.alt}
+                      src={`/home-page/event-locations/${item.src}`}
+                      height={303}
+                      width={463}
+                      className="opacity-60"
+                      priority={true}
+                    />
+                    <div className="card-content absolute inset-x-0 bottom-[-2px] bg-gradient-to-b from-transparent to-white px-6 pb-10 text-black dark:to-[#030711]">
+                      {item.heading === undefined ? (
+                        <div className="text-md flex pb-2 font-semibold tracking-wide md:text-lg">
+                          <span className="pr-2">
+                            <ChevronsRight />
+                          </span>{" "}
+                          {item.subtext}
+                        </div>
+                      ) : (
+                        <div className="pb-2 pl-4 pr-16 font-semibold md:pb-6">
+                          <div className="pb-4 text-xl font-bold sm:text-2xl md:text-3xl xl:text-4xl">
+                            {item.heading}
+                          </div>{" "}
+                          <div className="text-xs md:text-sm">
+                            {item.subtext}
+                          </div>
+                        </div>
+                      )}
+                    </div>
                   </div>
-                ) : (
-                  <div className="pb-2 pl-4 pr-16 font-semibold md:pb-6">
-                    <div className="pb-4 text-xl font-bold sm:text-2xl md:text-3xl xl:text-4xl">
-                      {item.heading}
-                    </div>{" "}
-                    <div className="text-xs md:text-sm">{item.subtext}</div>
-                  </div>
-                )}
-              </div>
-            </div>
-          ))}
+                </li>
+              </>
+            ))}
+          </ul>
         </div>
       ))}
     </div>
