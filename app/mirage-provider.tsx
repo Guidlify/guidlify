1d70bafb9f03dbd39ecb0b7902c1248db9ac7dac
Imam Ali Mustofa <d.darkterminal@gmail.com>
2024-01-13 19:04:50 +0700

    chore: remove unrelated model

--- app/mirage-provider.tsx
+++ app/mirage-provider.tsx
@@ -2,7 +2,6 @@
 
 import { useEffect } from "react"
 import { makeServer } from "@/migrate"
-import { movies } from "@/mock-models/movies"
 import { badgeSponsorData, eventData } from "@/mock-models/organization"
 import { SponsoringOrganizationsData } from "@/mock-models/sponsor"
 import { badgeSponsorEventsData, userEventData } from "@/mock-models/user"
@@ -10,7 +9,6 @@ import { createServer } from "miragejs"
 
 createServer({
   routes() {
-    this.get("/api/movies", () => movies)
     this.get("/api/organization", () => ({
       eventData,
       badgeSponsorData,
@@ -28,10 +26,6 @@ createServer({
 export default function MirageProvider() {
   useEffect(() => {
     ;async () => makeServer()
-    // COMMENT: Remove this examples
-    fetch("/api/movies")
-      .then((res) => res.json())
-      .then((json) => console.log(json.movies))
   }, [])
   return <></>
 }
