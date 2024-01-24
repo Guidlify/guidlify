e32ba04601a910fc4bab41b1479d21e366aa197f
Anmol Baranwal <anmolbaranwal119@gmail.com>
2024-01-24 17:19:17 +0530

    chore: add ui components along with react hook form and zod packages

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,25 +1,66 @@
 import * as React from "react"
+import { useState } from "react"
 
 import { cn } from "@/lib/utils"
+
+import { Icons } from "../icons"
 
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
+    const [isPasswordVisible, setIsPasswordVisible] = useState<boolean>(false)
+    const [inputValue, setInputValue] = useState<string>("")
+
+    const handleTogglePasswordVisibility = () => {
+      setIsPasswordVisible(!isPasswordVisible)
+    }
+
+    const getInputType = () => {
+      if (isPasswordVisible) {
+        return "text"
+      } else if (type === "password") {
+        return "password"
+      } else {
+        return type
+      }
+    }
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setInputValue(e.target.value)
+    }
+
     return (
-      <input
-        type={type}
-        className={cn(
-          "flex h-9 w-full rounded-md border border-input bg-background px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
-          className
+      <div className="relative">
+        {type === "search" && (
+          <Icons.search className="absolute left-5 top-3 mr-2 h-4 w-4 shrink-0 opacity-50" />
         )}
-        ref={ref}
-        {...props}
-      />
+        <input
+          type={type === "password" ? getInputType() : type}
+          className={cn(
+            "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+            className
+          )}
+          ref={ref}
+          value={inputValue}
+          onChange={handleInputChange}
+          {...props}
+        />
+        {type === "password" && inputValue && (
+          <button
+            type="button"
+            className="absolute inset-y-0 right-3 top-2 px-2"
+            onClick={handleTogglePasswordVisibility}
+          >
+            {isPasswordVisible ? <Icons.eyeOff /> : <Icons.eye />}
+          </button>
+        )}
+      </div>
     )
   }
 )
+
 Input.displayName = "Input"
 
 export { Input }
